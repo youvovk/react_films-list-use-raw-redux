40f8005efddf8c34a7f8007cf950b6061af8f87a
Yurii Holiuk <bionicuss@gmail.com>
2019-09-23 11:24:23 +0300

    add fetch

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import './App.scss';
 import { FilmsList } from './components/FilmsList';
 import { NewFilm } from './components/NewFilm';
 import { films } from './data';
+import { FormField } from './components/FormField';
+
+const API_URL = 'http://www.omdbapi.com/?apikey=2f4a38c9&t=';
 
 export class App extends Component {
   state = {
     filmsList: films,
+    searchWord: '',
   };
+
+  componentDidMount() {
+    this.searchFilm('spider');
+  }
 
   handleAddFilm = (newFilm) => {
     this.setState(prevState => ({
@@ -21,12 +29,56 @@ export class App extends Component {
     }));
   };
 
+  handleSearchChange = ({ target }) => {
+    this.setState({ searchWord: target.value });
+  };
+
+  searchFilm = (searchWord) => {
+    fetch(`${API_URL}${searchWord}`)
+      .then(response => response.json())
+      .then((data) => {
+        const {
+          Title,
+          Plot,
+          Poster,
+          Website,
+          imdbID,
+        } = data;
+
+        const newFilm = {
+          id: imdbID,
+          title: Title,
+          description: Plot,
+          imgUrl: Poster,
+          imdbUrl: Website,
+        };
+
+        this.setState(prevState => ({
+          filmsList: [...prevState.filmsList, newFilm],
+        }));
+      });
+  };
+
   render() {
-    const { filmsList } = this.state;
+    const { filmsList, searchWord } = this.state;
 
     return (
       <div className="page">
         <div className="content">
+          <FormField
+            value={searchWord}
+            name="searchWord"
+            placeholder="Type search word"
+            label="Search film"
+            onChange={this.handleSearchChange}
+          />
+          <button
+            onClick={() => this.searchFilm(searchWord)}
+            type="button"
+            className="button is-primary"
+          >
+            Search film
+          </button>
           <FilmsList films={filmsList} />
         </div>
         <div className="sidebar">
