35fcf48b3051f3c2185abd370cb1c618e9944a90
youvovk <wer3wollf@gmail.com>
2019-10-08 12:42:49 +0300

    solution

---
+++ precache-manifest.d14f39201bd0f67668a79376003ccaeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef083a0b136fe288682ba14a71beef8c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "651e62ceb55d64d5a00f",
+    "url": "./static/css/2.1ab5750d.chunk.css"
+  },
+  {
+    "revision": "758b5ce15dab3de822cd",
+    "url": "./static/css/main.a18045c2.chunk.css"
+  },
+  {
+    "revision": "651e62ceb55d64d5a00f",
+    "url": "./static/js/2.5252e273.chunk.js"
+  },
+  {
+    "revision": "758b5ce15dab3de822cd",
+    "url": "./static/js/main.4112171a.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
